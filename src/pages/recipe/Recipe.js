1689e152ef0a7a9a81d4873947cf470b0a041656
Lisa Wagner <lisawagner.online@gmail.com>
2022-05-27 16:50:04 -0700

    Clean up

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -10,24 +10,25 @@ export default function Recipe() {
 
   return (
     <div className="recipe-container">
-    <div className='recipe vintageFx'>
-      <div className="pattern">
-        {error && <p className='error' >{error}</p>}
-        {isPending && <p className='loading' >Loading...</p>}
-        {recipe && (
-          <>
-            <h2 className='page-title'>{recipe.title}</h2>
-            <p className='cook-time'>Takes {recipe.cookingTime} to cook.</p>
-            <ul>
-              {recipe.ingredients.map( (ingredient) => (
-                <li key={ingredient}>{ingredient}</li>
-              ))}
-            </ul>
-            <p className='instructions' >{recipe.instructions}</p>
-          </>
-        )}
-      </div>
-    </div>
+        <div className='recipe vintageFx'>
+          <div className="pattern">
+            {error && <p className='error' >{error}</p>}
+            {isPending && <p className='loading' >Loading...</p>}
+            {recipe && (
+              <>
+                <h2 className='page-title'>{recipe.title}</h2>
+                <p className='cook-time'>Takes {recipe.cookingTime} to cook.</p>
+                <ul>
+                  {recipe.ingredients.map( (ingredient) => (
+                    <li key={ingredient}>{ingredient}</li>
+                  ))}
+                </ul>
+                <p className='instructions' >{recipe.instructions}</p>
+              </>
+            )}
+          </div>
+        </div>
+   
     </div>
   )
 }
